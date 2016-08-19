d657d6991c16518429b638f291d69bf2b623b1a7
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:01:22 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 ﻿{
     'name': "Leave states in the calendar view",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['hr_holidays'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
